9a8c153df1b5f54ffea33349282e387c779c6ce8
SohelDeshmukh <101352279+SohelDeshmukh@users.noreply.github.com>
2022-06-27 00:24:45 +0530

    Add files via upload

---
+++ WPT_Assignments/31.05.22/Q1_2.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded",()=>{
+
+    const btn = document.querySelector("#btn");
+
+    btn.addEventListener('click',()=>{
+            console.log("Button is Working without Global Function");
+
+    });
+    
+
+});
